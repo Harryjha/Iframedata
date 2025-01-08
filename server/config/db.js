e7e2d5c11a1d9d12c30885f7ec2ee58ade04164f
Jatesh jha <jateshjha@Jateshs-MacBook-Pro.local>
2025-01-08 12:47:43 +0530

    iframe

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2/promise"); // Use promise-based mysql2
  
 const mysqlPool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "12345678",
+    host: "",
+    user: "",
+    password: "",
     database: "student_portal",
     waitForConnections: true,
     connectionLimit: 10,
